b4a0eb20f3216780e9944e27c14e44aac3a00074
ben-domingue <ben.domingue@gmail.com>
2022-04-08 10:11:28 -0700

    per our phone call

--- R/simfuns.R
+++ R/simfuns.R
@@ -93,3 +93,25 @@ integrate<-function(rt,caf) {
 
 
 
+getcaf0<-function (x,ran=NULL)  ##this computes an overall CAF
+{
+    library(splines)
+    tmp.bs <- bs(x$rt)
+    for (i in 1:ncol(tmp.bs)) x[[paste("bs", i, sep = "")]] <- tmp.bs[,i]
+    caf <- list()
+    library(fixest)
+    if (is.null(ran)) ran <- quantile(x$rt, c(0.01, 0.99), na.rm = TRUE)
+    xx <- x
+    mod <- feols(resp ~ bs1 + bs2 + bs3 | item + id, xx)
+    t <- seq(ran[1], ran[2], length.out = 1000)
+    z <- predict(tmp.bs, t)
+    #fe <- fixest::fixef(mod)
+    z <- data.frame(bs1 = z[, 1], bs2 = z[, 2], bs3 = z[,3])
+    co <- coef(mod)
+    #yhat <- 0
+    #for (j in 1:length(co)) yhat <- yhat + co[j] * z[, j]
+    #yhat <- yhat + mean(fe$item) + mean(fe$id)
+    yhat<-as.matrix(z) %*% matrix(co,ncol=1)
+    yhat<-yhat[,1]-mean(yhat[,1])
+    data.frame(t = t,  yhat = yhat)
+}
